68d7e0430812982c8febc58109103b2451e61f1f
Tim Stewart <tim@stoo.org>
2015-10-05 01:17:21 -0400

    Use only 1 UDP process, fix lager crash.log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/.nix-profile/bin/bash -x
 
 # Bring in Docker environment
-SERVERS="[[{name,inet_localhost_1},{address,\"$BIND_ADDRESS\"},{port,$BIND_PORT},{family,inet},{processes,2}]]"
+SERVERS="[[{name,inet_1},{address,\"$BIND_ADDRESS\"},{port,$BIND_PORT},{family,inet}]]"
 
 exec privbind -u erldns erldns "$@" -erldns servers "$SERVERS"
